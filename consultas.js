7dc486025d26667de6695f72e19387682ace4647
Katherine Stehberg <kstehberg@gmail.com>
2024-08-06 16:34:39 -0400

    consultas commented

--- consultas.js
+++ consultas.js
@@ -1,7 +1,7 @@
 // consultas.js
-const verificarCredenciales = async (email, password) => {
-    const consulta = "SELECT * FROM usuarios WHERE email = $1 AND password = $2"
-    const values = [email, password]
-    const { rowCount } = await pool.query(consulta, values)
-    if (!rowCount) throw { code: 404, message: "No se encontró ningún usuario con estas credenciales" }
-   }
+//const verificarCredenciales = async (email, password) => {
+   // const consulta = "SELECT * FROM usuarios WHERE email = $1 AND password = $2"
+    //const values = [email, password]
+    //const { rowCount } = await pool.query(consulta, values)
+    //if (!rowCount) throw { code: 404, message: "No se encontró ningún usuario con estas credenciales" }
+  // }
--- middlewares/serverLog.js
+++ middlewares/serverLog.js
@@ -1,9 +1,9 @@
-export const serverLog = (req, _, next) => {
-    console.log({
-        method: req.method,
-        body: req.body,
-        params: req.params,
-        query: req.query,
-    });
-    next();
-};
+//export const serverLog = (req, _, next) => {
+   // console.log({
+       // method: req.method,
+        //body: req.body,
+        //params: req.params,
+        //query: req.query,
+    //});
+    //next();
+//};
